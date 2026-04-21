e7e129e99a5ac32584659fcf7a901e79c3ac0f2a
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-04-21 21:20:05 +0300

    Add Upload Existing button to Dashboard for enhanced user functionality

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import { Plus, PlusIcon } from 'lucide-react'
+import { Plus, PlusIcon, UploadCloudIcon } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
@@ -16,7 +16,18 @@ const Dashboard = () => {
             from-indigo-300 to-indigo-500 text-white rounded-full'/>
             <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
           </button>
+          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center
+          rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500
+          hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br
+            from-purple-300 to-purple-500 text-white rounded-full'/>
+            <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing</p>
+          </button>
         </div>
+
+        <hr className='border-slate-300 my-6 sm:w-[305px]'/>
+
+
       </div>
     
     </div>
