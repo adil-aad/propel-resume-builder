80a54f138f20dbab795cde691512b13b117dd41c
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-04-24 21:13:07 +0300

    Add edit resume title functionality to Dashboard for improved user experience

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -166,6 +166,23 @@ const Dashboard = () => {
           )
         }
 
+        {
+          editResumeId && (
+            <form onSubmit={editTitle} onClick={()=> setEditResumeId('')} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 
+            z-10 flex items-center justify-center'>
+              <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+                <h2 className='text-xl font-bold mb-4'>Edit Resume Title</h2>
+                <input onChange={(e)=> setTitle(e.target.value)} value={title} type="text" placeholder='Enter Resume Title' className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required />
+                <button type="submit" className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700
+                transition-colors'>Update</button>
+                <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600
+                cursor-pointer transition-colors' onClick={()=>{setEditResumeId(false); setTitle('')}}/>
+              </div>
+
+            </form>
+          )
+        }
+
         <div>
 
         </div>
