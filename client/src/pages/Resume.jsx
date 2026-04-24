e3062ed78fb9e2a6ab174bb9f7f27a795bf828a2
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-04-24 21:50:49 +0300

    Add resume loading functionality to display existing resume data

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
 
 const Resume = () => {
+
+  const {resumeId} = useParams()
+
+  const [resumeData, setResumeData] = useState({
+    _id: '',
+    title: '',
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  }) 
+
+  const loadExsistingResume = async () => {
+    const resume = dummyResumeData.find(resume => resume._id === resumeId)
+    if (resume) {
+      setResumeData(resume)
+      document.title = resume.title
+    }
+  }
+
+  useEffect(()=>{
+    loadExsistingResume()
+  }, [])
   return (
-    <div>Resume Page</div>
+    <div>
+
+    </div>
   )
 }
 
