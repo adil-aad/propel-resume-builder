759140da55c820f13c50d2d7421752d201faee63
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-05-08 21:14:36 +0300

    Implement resume preview functionality and add Loading component for better user experience

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
 
 const Preview = () => {
+
+  const {resumeId} = useParams()
+
+  const [resumeData, setResumeData] = useState(null)
+
+  const loadResume = async () => {
+    setResumeData(dummyResumeData.find((resume => resume._id === resumeId)))
+  }
+
+  useEffect(()=>{
+    loadResume()
+  },[])
   return (
-    <div>Preview Page</div>
+    <div>
+
+    </div>
   )
 }
 
