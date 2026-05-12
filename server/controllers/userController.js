1b36599e4f7af71226fd07629185668cb517613a
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-05-12 21:19:53 +0300

    fix(controller): correct user login logic and improve error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,10 +34,35 @@ export const registerUser = async (req, res) => {
         newUser.password = undefined
 
         return res.status(201).json({message: 'User Created successfully', token, user: newUser})
-        
+                
+
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
 
 
-        
+// User Login
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password} = req.body
+
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({message: "Invalid Email or Password"})
+        }
+
+        if(!user.comparePassword(password)){
+            return res.status(400).json({message: "Invalid Email or Password"}) 
+        }
+
+        const token = generateToken(user._id)
+
+        user.password = undefined
+
+        return res.status(200).json({message: 'Login successful', token, user})
+                
 
     } catch (error) {
         return res.status(400).json({message: error.message})
