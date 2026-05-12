8216ff293ee9d816bb9a07ed1b34890f772b88b5
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-05-12 21:15:17 +0300

    feat(controller): refactor user registration logic to include token generation and fix password hashing variable name

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,13 @@
 import User from '../models/User.js'
 import bcrypt from 'bcrypt'
+import { response } from 'express'
+import jwt from 'jsonwebtoken'
 
 
-
+const generateToken = (userId) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'})
+    return token
+}
 
 
 export const registerUser = async (req, res) => {
@@ -18,17 +23,23 @@ export const registerUser = async (req, res) => {
             return res.status(400).json({message: "User already Exists"})
         }
 
-        const hasedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)
 
         const newUser = await User.create({
-            name, email, password: hasedPassword
+            name, email, password: hashedPassword
         })
+
+        const token = generateToken(newUser._id)
+
+        newUser.password = undefined
+
+        return res.status(201).json({message: 'User Created successfully', token, user: newUser})
         
 
 
         
 
     } catch (error) {
-        
+        return res.status(400).json({message: error.message})
     }
 }
