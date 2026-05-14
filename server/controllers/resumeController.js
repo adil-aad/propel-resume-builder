d14fdc3331414d41779a783054e235bd5d5e772b
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-05-14 20:58:15 +0300

    fix(controller): correct error handling in createResumes and deleteResume functions

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -17,3 +17,23 @@ export const createResumes = async (req, res) => {
         return res.status(400).json({message: error.message})
     }
 }
+
+
+
+export const deleteResume = async (req, res) => {
+    try {
+        const userId = req.userId
+
+        const {resumeId} = req.params
+
+        // delete resume
+
+        await Resume.findOneAndDelete({userId, _id: resumeId})
+
+
+        return res.status(200).json({message: "Resume deleted Succesfully"})
+
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
