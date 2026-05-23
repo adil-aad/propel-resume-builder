0d88c10c971878f47d767f4e945801bcd1766a96
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-05-23 21:34:35 +0300

    fix(routes): declare userRouter as a constant for proper initialization

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { getUserById, getUserResumes, loginUser, registerUser } from '../controllers/userController.js'
 import protect from '../middlewares/authMiddleware.js'
 
-userRouter = express.Router()
+const userRouter = express.Router()
 
 
 userRouter.post('/register', registerUser)
