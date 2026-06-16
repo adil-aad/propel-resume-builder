23ac1a819fb4da495ba6e69fefe0bd736f7df6a7
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-06-16 11:05:50 +0300

    feat(aiController): add uploadResume function for extracting and saving structured resume data

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,4 +1,5 @@
 import ai from "../configs/ai.js"
+import Resume from '../models/Resume.js'
 
 export const enhanceSummary = async (req, res) => {
     try {
@@ -34,7 +35,7 @@ export const enhanceJobDescription = async (req, res) => {
         const {userContent} = req.body
 
         if(!userContent){
-            return res.status(400).json({message: "Fill in the required fields"})
+            return res.status(400).json({message: 'Fill in the Required Fileds'})
         }
 
         const response = await ai.models.generateContent({
@@ -42,7 +43,7 @@ export const enhanceJobDescription = async (req, res) => {
         contents: userContent,
         config: {
          systemInstruction:
-            "You are a professional resume writer. Improve the user's job description while keeping it truthful, concise, ATS-friendly, and professional. Return only the enhanced summary.",
+            "You are a professional resume writer. Improve the user's job description while keeping it truthful, concise, ATS-friendly, and professional. Return only the enhanced job description",
         },
         });
 
@@ -53,4 +54,86 @@ export const enhanceJobDescription = async (req, res) => {
     } catch (error) {
         return res.status(400).json({message: error.message})
     }
+}
+
+
+// controller for uploading the resume to the database
+
+
+export const uploadResume = async (req, res) => {
+  try {
+    const { resumeText, title } = req.body
+    const userId = req.userId
+
+    if (!resumeText) {
+      return res.status(400).json({ message: "Fill in the required fields" })
+    }
+
+    const systemPrompt =
+      "You are an expert resume parser. Extract structured resume data from the user's resume. Return only valid JSON. Do not include markdown, explanations, or extra text."
+
+    const userPrompt = `
+    Extract the resume data from this resume text:
+
+    ${resumeText}
+
+    Return JSON with this exact structure without any text before or after it:
+
+
+    proffesionlaSummary: {type: String, default: ''},
+    skills: [{type: String}],
+    perosonalInfo: {
+        image: {type: String, default: ''},
+        full_name: {type: String, default: ''},
+        proffesion: {type: String, default: ''},
+        email: {type: String, default: ''},
+        phone: {type: String, default: ''},
+        location: {type: String, default: ''},
+        linkedin: {type: String, default: ''},
+        website: {type: String, default: ''},
+    }, 
+    experience:[
+        {
+            company: {type: String},
+            position: {type: String},
+            start_date: {type: String},
+            end_date: {type: String},
+            description: {type: String},
+            is_current: {type: Boolean},
+        }
+    ],
+    project: [
+        {
+            name: {type: String},
+            type: {type: String},
+            description: {type: String},
+        }
+    ],
+    education:[
+        {
+            institution: {type: String},
+            degree: {type: String},
+            field: {type: String},
+            graduation_date: {type: String},
+            gpa: {type: String},
+        }
+    `
+
+    const response = await ai.models.generateContent({
+      model: process.env.GEMINI_MODEL,
+      contents: userPrompt,
+      config: {
+        systemInstruction: systemPrompt,
+        responseMimeType: "application/json"
+      },
+    })
+
+    const extractedResume = response.text
+    const parsedData = JSON.parse(extractedResume)
+    const newResume = await Resume.create({userId, title, ...parsedData})
+
+    return res.json({ resumeId: newResume._id })
+  } catch (error) {
+    return res.status(400).json({ message: error.message })
+  }
 }
