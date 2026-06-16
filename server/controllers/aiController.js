e9ba4322d249d3ee6c0bc9abf642e85204bc1161
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-06-16 11:08:57 +0300

    feat(aiController): update uploadResume function to return structured JSON format for resume data

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -80,43 +80,46 @@ export const uploadResume = async (req, res) => {
     Return JSON with this exact structure without any text before or after it:
 
 
-    proffesionlaSummary: {type: String, default: ''},
-    skills: [{type: String}],
-    perosonalInfo: {
-        image: {type: String, default: ''},
-        full_name: {type: String, default: ''},
-        proffesion: {type: String, default: ''},
-        email: {type: String, default: ''},
-        phone: {type: String, default: ''},
-        location: {type: String, default: ''},
-        linkedin: {type: String, default: ''},
-        website: {type: String, default: ''},
-    }, 
-    experience:[
-        {
-            company: {type: String},
-            position: {type: String},
-            start_date: {type: String},
-            end_date: {type: String},
-            description: {type: String},
-            is_current: {type: Boolean},
-        }
-    ],
-    project: [
-        {
-            name: {type: String},
-            type: {type: String},
-            description: {type: String},
-        }
-    ],
-    education:[
-        {
-            institution: {type: String},
-            degree: {type: String},
-            field: {type: String},
-            graduation_date: {type: String},
-            gpa: {type: String},
-        }
+    {
+        "proffesionlaSummary": "",
+        "skills": [],
+        "perosonalInfo": {
+            "image": "",
+            "full_name": "",
+            "proffesion": "",
+            "email": "",
+            "phone": "",
+            "location": "",
+            "linkedin": "",
+            "website": ""
+        },
+        "experience": [
+            {
+            "company": "",
+            "position": "",
+            "start_date": "",
+            "end_date": "",
+            "description": "",
+            "is_current": false
+            }
+        ],
+        "project": [
+            {
+            "name": "",
+            "type": "",
+            "description": ""
+            }
+        ],
+        "education": [
+            {
+            "institution": "",
+            "degree": "",
+            "field": "",
+            "graduation_date": "",
+            "gpa": ""
+            }
+        ]
+    }
     `
 
     const response = await ai.models.generateContent({
