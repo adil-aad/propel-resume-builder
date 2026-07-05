db4fae3ab392a33af3a45eb2a3a1ca32cb540486
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-07-05 22:14:29 +0300

    fix(Dashboard, resumeController): improve loading state management and ensure proper resume data handling during updates

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -60,19 +60,39 @@ const Dashboard = () => {
       navigate(`/app/builder/${data.resumeId}`)
     } catch (error) {
       toast.error(error?.response?.data?.message || error.message)
-    }    
-    setIsLoading(false)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   const editTitle = async (event) => {
-    event.preventDefault()
+    
+    try {
+      event.preventDefault()
+      const { data } = await api.put('/api/resumes/update', {resumeId: editResumeId,
+        resumeData: {title}}, {headers: {Authorization: token}})
+      setAllResumes(prev => prev.map(resume => resume._id === editResumeId ? {...resume, title}: resume))
+      setTitle('')
+      setEditResumeId('')
+      toast.success(data.message)
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message)
+    }
   }
 
   const deleteResume = async (resumeId) => {
-    const confirm = window.confirm('Are you sure you want to delete this resume?')
-    if (confirm) {
-      setAllResumes(prev => prev.filter(resume => resume._id !== resumeId))
+
+    try {
+      const confirm = window.confirm('Are you sure you want to delete this resume?')
+      if (confirm) {
+        const { data } = await api.delete(`/api/resumes/delete/${resumeId}`, {headers: {Authorization: token}})
+        setAllResumes(prev => prev.filter(resume => resume._id !== resumeId))
+        toast.success(data.message)
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message)
     }
+    
   }
 
   useEffect(() => {
